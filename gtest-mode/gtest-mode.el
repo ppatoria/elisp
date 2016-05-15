15d11f45651b5b55969294a39801fb0c9d9aff33
ppatoria <ppatoria@gmail.com>
2016-05-15 16:43:00 +0200

    extended gtest-mode to run test from source code

--- gtest-mode/gtest-mode.el
+++ gtest-mode/gtest-mode.el
@@ -1,5 +1,5 @@
 (defgroup gtest nil
-  "gtest"	
+  "gtest group"	
   :group 'tools)
 
 
@@ -9,12 +9,12 @@
   :type 'string)
 
 (defun gtest-list ()
-  "This will list all the tests as per target path set in the global variable"
+  "List all the tests"
   (interactive)
   (shell-command (concat gtest-target " --gtest_list_tests" "&")))
 
 (defun gtest-run(filter)
-  "Run gtests using the filter passed as parameter"
+  "Run gtest as per filter"
   (interactive "sFilter: ")
   (shell-command (concat gtest-target " --gtest_filter=" filter "&")))
 
@@ -23,15 +23,50 @@
   (interactive)
   (string-match "[.]" (thing-at-point 'line t)))
 
-(defun search-test-at-point ()
-  ""
+(defun is-c++-mode ()
+  "check if its a c++ mode"
   (interactive)
-  (setq test (thing-at-point 'symbol t))
-  (if (is-line-at-point-is-test-hierarchy-or-fixture)
-      (concat test "*")
-    (re-search-backward "^[_[:alnum:]]+[.]")
-    (setq test-hierarchy-or-fixture (thing-at-point 'symbol t))
-    (concat test-hierarchy-or-fixture "." test)))
+  (eq 'c++-mode (buffer-local-value 'major-mode (current-buffer))))
+
+
+(defun parse-test ()
+  (interactive)
+  (setq test (thing-at-point 'line t))
+  (when (string-match "TEST.*" test)
+    (replace-regexp-in-string
+     " "
+     ""
+     (replace-regexp-in-string
+      ",[ ]*"
+      "."
+      (replace-regexp-in-string
+       "[)]"
+       ""
+       (replace-regexp-in-string
+	"TEST.*[(]"
+	""
+	test))))))
+  
+(defun test-parse-test ()
+  (interactive)
+  (message "%s" (parse-test)))
+
+(defun search-test-at-point-in-source-file ()
+  (interactive)
+  (parse-test))
+
+  
+(defun search-test-at-point ()
+  "search test at pointsearch test at point"
+  (interactive)
+  (if (is-c++-mode)
+      (search-test-at-point-in-source-file)
+    (setq test (thing-at-point 'symbol t))
+    (if (is-line-at-point-is-test-hierarchy-or-fixture)
+	(concat test "*")
+      (re-search-backward "^[_[:alnum:]]+[.]")
+      (setq test-hierarchy-or-fixture (thing-at-point 'symbol t))
+      (concat test-hierarchy-or-fixture "." test))))
 
 (defun gtest-run-at-point ()
   "run test at point"
